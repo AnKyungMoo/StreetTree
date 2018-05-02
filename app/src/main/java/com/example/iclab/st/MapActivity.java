90b65d4b715454f8ceaa8989bcc2b0b5b6ee89ac
KM <sodp2@naver.com>
2018-05-03 01:13:17 +0900

    GPS를 이용한 현재 위치 버튼 기능 추가

--- app/src/main/java/com/example/iclab/st/MapActivity.java
+++ app/src/main/java/com/example/iclab/st/MapActivity.java
@@ -1,9 +1,18 @@
 package com.example.iclab.st;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 
 import net.daum.mf.map.api.MapPOIItem;
@@ -11,7 +20,12 @@ import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
 
 // 지도 액티비티
-public class MapActivity extends AppCompatActivity implements MapView.MapViewEventListener{
+public class MapActivity extends AppCompatActivity implements MapView.MapViewEventListener {
+    Button gpsButton = null;
+    MapView mapView = null;
+    MapPoint mapPoint = null;
+    double latitude;
+    double longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +38,38 @@ public class MapActivity extends AppCompatActivity implements MapView.MapViewEve
         container.addView(mapView);
 
         mapView.setMapViewEventListener(this);
+
+        gpsButton = (Button) findViewById(R.id.gps);
+
+        gpsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                moveMapViewCurrentPosition();
+            }
+        });
     }
 
     @Override
     public void onMapViewInitialized(MapView mapView) {
-        MapPoint mapPoint = MapPoint.mapPointWithGeoCoord(36.770598, 126.931647);
+        this.mapView = mapView;
+        mapPoint = MapPoint.mapPointWithGeoCoord(36.770598, 126.931647);
+        this.mapView.setMapCenterPoint(mapPoint, true);
+    }
+
+    public void moveMapViewCurrentPosition() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager.removeUpdates(locationListener);
+
+        // 권한이 허용되어있지 않은 경우
+        if (ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            /* TODO: 권한 부여 */
+            return;
+        }
+        locationManager.requestLocationUpdates("gps", 0, 0, locationListener);
+
+        mapPoint = MapPoint.mapPointWithGeoCoord(latitude, longitude);
         mapView.setMapCenterPoint(mapPoint, true);
     }
 
@@ -87,4 +128,31 @@ public class MapActivity extends AppCompatActivity implements MapView.MapViewEve
     public void onMapViewMoveFinished(MapView mapView, MapPoint mapPoint) {
 
     }
+
+    private final LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+            latitude = location.getLatitude();   //위도
+            longitude = location.getLongitude(); //경도
+
+            locationManager.removeUpdates(locationListener);
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+    };
 }
