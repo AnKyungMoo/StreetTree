6258f8e917fe697240351ecc2afff060a100889b
KM <sodp2@naver.com>
2018-05-02 11:35:20 +0900

    activiry_map에 다음 지도 api 적용

--- app/src/main/java/com/example/iclab/st/MapActivity.java
+++ app/src/main/java/com/example/iclab/st/MapActivity.java
@@ -2,13 +2,80 @@ package com.example.iclab.st;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+
+import net.daum.mf.map.api.MapPOIItem;
+import net.daum.mf.map.api.MapPoint;
+import net.daum.mf.map.api.MapView;
 
 // 지도 액티비티
-public class MapActivity extends AppCompatActivity {
+public class MapActivity extends AppCompatActivity implements MapView.MapViewEventListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        MapView mapView = new MapView(this);
+        mapView.setDaumMapApiKey("e95ede72416f09346c75c0acb52472ed");
+        RelativeLayout container = (RelativeLayout) findViewById(R.id.map);
+        container.addView(mapView);
+
+        mapView.setMapViewEventListener(this);
+    }
+
+    @Override
+    public void onMapViewInitialized(MapView mapView) {
+        MapPoint mapPoint = MapPoint.mapPointWithGeoCoord(36.770598, 126.931647);
+        mapView.setMapCenterPoint(mapPoint, true);
+    }
+
+    @Override
+    public void onMapViewCenterPointMoved(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewZoomLevelChanged(MapView mapView, int i) {
+
+    }
+
+    @Override
+    public void onMapViewSingleTapped(MapView mapView, MapPoint mapPoint) {
+        /*
+        MapPOIItem marker = new MapPOIItem();
+        marker.setItemName("SoonChunHyaung");
+        marker.setTag(0);
+        marker.setMapPoint(mapPoint);
+        marker.setMarkerType(MapPOIItem.MarkerType.BluePin); // 기본으로 제공하는 BluePin 마커 모양.
+        marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin); // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
+
+        mapView.addPOIItem(marker);
+        */
+    }
+
+    @Override
+    public void onMapViewDoubleTapped(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewLongPressed(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewDragStarted(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewDragEnded(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewMoveFinished(MapView mapView, MapPoint mapPoint) {
+
     }
 }
