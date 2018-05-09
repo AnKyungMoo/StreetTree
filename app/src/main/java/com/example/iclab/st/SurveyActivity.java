ca651ffe39e01604fc77ee8fe35ac211f3b08d75
KM <sodp2@naver.com>
2018-05-09 21:54:32 +0900

    intent 변수명 변경

--- app/src/main/java/com/example/iclab/st/SurveyActivity.java
+++ app/src/main/java/com/example/iclab/st/SurveyActivity.java
@@ -16,10 +16,10 @@ public class SurveyActivity extends AppCompatActivity {
         setContentView(R.layout.activity_survey);
 
         // 위도 경도 좌표 값
-        Intent intent = getIntent();
+        Intent preIntent = getIntent();
 
-        double latitude = intent.getDoubleExtra("latitude", 0.0f);
-        double longitude = intent.getDoubleExtra("longitude", 0.0f);
+        double latitude = preIntent.getDoubleExtra("latitude", 0.0f);
+        double longitude = preIntent.getDoubleExtra("longitude", 0.0f);
 
         Log.d("latitude", latitude + "");
         Log.d("longitude", longitude + "");
