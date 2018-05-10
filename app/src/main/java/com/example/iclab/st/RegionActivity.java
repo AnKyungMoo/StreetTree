a6aa472f86103f638d67a6ad70f8db8a137bad1d
hyeenee <haeneeee@gmail.com>
2018-05-10 22:17:59 +0900

    RegionActivity 추가

---
+++ app/src/main/java/com/example/iclab/st/RegionActivity.java
@@ -0,0 +1,27 @@
+package com.example.iclab.st;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class RegionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_region);
+
+        Button backBtn = (Button)findViewById(R.id.backBtn);
+
+        // 뒤로 버튼 누르면 현장명입력 화면으로 다시 이동
+        backBtn.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), NewplaceActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
