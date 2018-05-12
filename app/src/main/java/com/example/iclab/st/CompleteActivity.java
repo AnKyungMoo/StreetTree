235ff5372d3624ba0d5c47f6fd0f64b2f9337468
hyeenee <haeneeee@gmail.com>
2018-05-13 02:26:41 +0900

    실측 결과 출력 후 FunctionActivity로 이동하는 버튼 기능 추가

--- app/src/main/java/com/example/iclab/st/CompleteActivity.java
+++ app/src/main/java/com/example/iclab/st/CompleteActivity.java
@@ -1,7 +1,10 @@
 package com.example.iclab.st;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 // 실측완료를 누르면 최종 결과 값이 출력되는 액티비티
 public class CompleteActivity extends AppCompatActivity {
@@ -10,5 +13,16 @@ public class CompleteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_complete);
+
+        Button resultBtn = (Button)findViewById(R.id.resultBtn);
+
+        // 완료 버튼 누르면 기능선택 화면으로 다시 이동
+        resultBtn.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), FunctionActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
