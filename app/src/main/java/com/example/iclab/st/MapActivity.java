39aa45509b78dccd932ac1ac5ae7993466b5bcec
KM <sodp2@naver.com>
2018-08-02 19:37:50 +0900

    지도에서 마커 찍은 위치의 동 코드와 지역명 체크

--- app/src/main/java/com/example/iclab/st/MapActivity.java
+++ app/src/main/java/com/example/iclab/st/MapActivity.java
@@ -4,18 +4,30 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import net.daum.mf.map.api.MapPOIItem;
 import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.ExecutionException;
 
 // 지도 액티비티
 public class MapActivity extends AppCompatActivity implements MapView.MapViewEventListener {
@@ -139,6 +151,20 @@ public class MapActivity extends AppCompatActivity implements MapView.MapViewEve
 
             mapView.addPOIItem(marker);
 
+            double latitude = mapPoint.getMapPointGeoCoord().latitude;
+            double longitude = mapPoint.getMapPointGeoCoord().longitude;
+
+            /***************************************************************************
+            *  dongCode가 현재 동의 코드를 나타냅니다.
+            *  getDongCode는 동의 코드를 가져오는 소스코드인데,
+            *  건물 안의 경우 충청남도와 같이 '도' 단위의 위치가 파악이 안되는 이슈가 있는데
+            *  해결의 방안을 모르겠네욤..
+            *  getAddress로 국가 도 시 동 ex) 대한민국 충청남도 아산시 신창면 의 형태로
+            *  String을 가져올 수 있습니다.
+            *  필요하시다면 substring을 이용하여서 대한민국 잘라서 사용하세욥
+            ***************************************************************************/
+            int dongCode = getDongCode(getAddress(this, latitude, longitude));
+
             // 버튼 활성화
             applyButton.setVisibility(View.VISIBLE);
             cancelButton.setVisibility(View.VISIBLE);
@@ -199,4 +225,81 @@ public class MapActivity extends AppCompatActivity implements MapView.MapViewEve
 
         }
     };
+
+    // 현재 위치 획득
+    private String getAddress(Context context, double lat, double lon) {
+
+        Geocoder geocoder = new Geocoder(context);
+        List<Address> location = null;
+        try {
+            location = geocoder.getFromLocation(lat, lon, 1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String address = location.get(0).getAddressLine(0);
+
+        Log.d("address:", address);
+
+        return address;
+    }
+
+    // 현재 동의 코드 획득
+    private int getDongCode(String address)
+    {
+        int dongCode = 0;
+        String[] region;
+
+        region = address.split("\\s");
+
+        for (int i = 0; i < region.length; ++i)
+        {
+            Log.d("region: ", region[i]);
+        }
+
+        String[] urls = new String[4];
+        String jsonText = ".json.txt";
+
+        // 지역 URL
+        urls[1] = "http://www.kma.go.kr/DFSROOT/POINT/DATA/top.json.txt";
+        urls[2] = "http://www.kma.go.kr/DFSROOT/POINT/DATA/mdl.";
+        urls[3] = "http://www.kma.go.kr/DFSROOT/POINT/DATA/leaf.";
+
+        try {
+
+            // 현재 지역의 지역 코드를 검색
+            for (int i = 1; i <= urls.length; ++i)
+            {
+                LinkedHashMap<String, Integer> regionMap = new LinkedHashMap<String, Integer>();
+                URL url = new URL(urls[i]);
+
+                URLAsyncTask regionTask = new URLAsyncTask();
+
+                regionTask.execute(url);
+
+                regionMap = regionTask.get();
+
+                if (i == 3)
+                {
+                    // 동코드: region[3]
+                    Log.d("region code: ", regionMap.get(region[i]) + "");
+                    dongCode = regionMap.get(region[i]);
+                    break;
+                }
+
+                urls[i + 1] += String.valueOf(regionMap.get(region[i]));
+                urls[i + 1] += jsonText;
+
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        return dongCode;
+    }
 }
