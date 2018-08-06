ee52978db3ecffb696b02932d908b7d47ac91d3c
Unknown <abdh94@naver.com>
2018-08-06 17:05:46 +0900

    기존현장체크 구현
    실측 값 리스트까지

--- app/src/main/java/com/example/iclab/st/ValueprintActivity.java
+++ app/src/main/java/com/example/iclab/st/ValueprintActivity.java
@@ -1,8 +1,17 @@
 package com.example.iclab.st;
 
 // 실측값 리스트 출력 액티비티 (지역으로 검색, 현장명으로 검색하고 이동)
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.example.iclab.st.NamesrchActivity.newCS;
 
 public class ValueprintActivity extends AppCompatActivity {
 
@@ -10,5 +19,25 @@ public class ValueprintActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_valueprint);
+        Intent intent = getIntent();
+        final ListView vList = findViewById(R.id.valueList);
+        List<String> listinfo=new ArrayList<>();
+        final ArrayAdapter<String> listAdapter = new ArrayAdapter<String> (this, android.R.layout.simple_list_item_1, listinfo);
+
+        int pos = -1;
+        if(intent.getExtras() != null)
+        {
+             pos = intent.getIntExtra("position",-1);
+        }
+        for(int i=0; i< newCS.get(pos).list.size();i++)
+        {
+            SurveyList sl = newCS.get(pos).list.get(i);
+            String s = "NO."+(i+1)+ "   보호판 : "+sl.plateName +"\n위도 : "+sl.latitude+"  \n경도 : "+sl.longitude;
+            listinfo.add(s);
+        }
+
+        vList.setAdapter(listAdapter);
+
+
     }
 }
