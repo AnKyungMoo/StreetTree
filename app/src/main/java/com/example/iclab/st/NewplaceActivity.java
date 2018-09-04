ff0a29f8d56004bb256f1ccdd89e6e971fb29dd7
hyeenee <haeneeee@gmail.com>
2018-09-05 00:50:52 +0900

    newplaceActivity 입력값 업체 요청사항 반영(영업담당자, 관급사급, 납품예정일, 공차값)

--- app/src/main/java/com/example/iclab/st/NewplaceActivity.java
+++ app/src/main/java/com/example/iclab/st/NewplaceActivity.java
@@ -1,6 +1,10 @@
 package com.example.iclab.st;
 
 import android.annotation.SuppressLint;
+import android.app.DialogFragment;
+import android.app.Dialog;
+import android.app.DatePickerDialog;
+import android.widget.DatePicker;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -12,9 +16,9 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 // 신규현장실측(현장명입력) 액티비티
@@ -25,6 +29,7 @@ public class NewplaceActivity extends AppCompatActivity {
     EditText inputHyunjang;
     EditText inputBalju;
     TextView contentTxV;
+    protected static TextView displayDate;
     long now = System.currentTimeMillis();
     Date date;
     String nowDate;
@@ -35,6 +40,8 @@ public class NewplaceActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_newplace);
 
+        Button datePick = findViewById(R.id.datePick);
+        displayDate = findViewById(R.id.inputDelivery);
         Button saveBtn = findViewById(R.id.save);
         final Button okBtn = findViewById(R.id.ok);
         inputHyunjang = findViewById(R.id.inputHyunjang);
@@ -43,6 +50,14 @@ public class NewplaceActivity extends AppCompatActivity {
         date = new Date(now);
         inputHyunjang.setPrivateImeOptions("defaultInputmode=korean; ");
         inputBalju.setPrivateImeOptions("defaultInputmode=korean; ");
+
+        datePick.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                DatePickerFragment mDatePicker = new DatePickerFragment();
+                mDatePicker.show(getFragmentManager(), "select date");
+            }
+        });
+
 
         inputBalju.setImeOptions(EditorInfo.IME_ACTION_DONE);
         inputBalju.setOnEditorActionListener(new TextView.OnEditorActionListener() {
@@ -62,12 +77,13 @@ public class NewplaceActivity extends AppCompatActivity {
         nowDate = sdfNow.format(date);
 
 
-        // 레이아웃 배치를 위해 임시로 텍스트 띄우는 기능만 (담당자X// - 아직 로그인 구현X) // 0730 로그인 구현
+        // 레이아웃 배치를 위해 임시로 텍스트 띄우는 기능만 (담당자X// - 아직 로그인 구현X) // 0730 로그인 구현 // 출력부분 수정 필요
         okBtn.setOnClickListener(new Button.OnClickListener() {
             @SuppressLint("SetTextI18n")
             public void onClick(View v) {
-                contentTxV.setText("  현장명 :  " + inputHyunjang.getText() +"\n\n" + "  발주처 :  " + inputBalju.getText()
-                        +"\n\n" + "  날짜 :  " + nowDate+ "\n\n" + "  담당자 :  "+GCSurvey.authorFullName);
+                contentTxV.setText("  현장명 :  " + inputHyunjang.getText() +"\n" + "  발주처 :  " + inputBalju.getText()
+                        +"\n" + "  날짜 :  " + nowDate+ "\n" + "  담당자 :  "+GCSurvey.authorFullName + "\n" + "  영업담당자 :  " + "\n"
+                        + "  직급 :  " + "\n" + "  납품예정일 :  " + "\n" + "  공차값 :  ");
             }
         });
         // 저장 버튼 누르면 지도 화면으로 전환
@@ -82,6 +98,23 @@ public class NewplaceActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    public static class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            displayDate.setText(String.valueOf(year) + "-" + String.valueOf(month+1) + "-" + String.valueOf(day));
+
+        }
     }
 }
 
